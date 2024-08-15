9c92e618d659becf6b221583e2adab30199e9129
Y0-0N63 <boyun1625@naver.com>
2024-08-15 13:39:37 +0900

    달력 디자인이 깨지는 오류 해결

---
+++ src/main/java/schedule/schedule.java
@@ -0,0 +1,104 @@
+package schedule;
+
+public class schedule {
+    private String START_Y;
+    private String START_M;
+    private String START_D;
+    private String START_DY;
+    private String END_Y;
+    private String END_M;
+    private String END_D;
+
+    public String getSTART_Y() {
+        return START_Y;
+    }
+
+    public void setSTART_Y(String START_Y) {
+        this.START_Y = START_Y;
+    }
+
+    public String getSTART_M() {
+        return START_M;
+    }
+
+    public void setSTART_M(String START_M) {
+        this.START_M = START_M;
+    }
+
+    public String getSTART_D() {
+        return START_D;
+    }
+
+    public void setSTART_D(String START_D) {
+        this.START_D = START_D;
+    }
+
+    public String getSTART_DY() {
+        return START_DY;
+    }
+
+    public void setSTART_DY(String START_DY) {
+        this.START_DY = START_DY;
+    }
+
+    public String getEND_Y() {
+        return END_Y;
+    }
+
+    public void setEND_Y(String END_Y) {
+        this.END_Y = END_Y;
+    }
+
+    public String getEND_M() {
+        return END_M;
+    }
+
+    public void setEND_M(String END_M) {
+        this.END_M = END_M;
+    }
+
+    public String getEND_D() {
+        return END_D;
+    }
+
+    public void setEND_D(String END_D) {
+        this.END_D = END_D;
+    }
+
+    public String getEND_DY() {
+        return END_DY;
+    }
+
+    public void setEND_DY(String END_DY) {
+        this.END_DY = END_DY;
+    }
+
+    public String getSCH_TYPE() {
+        return SCH_TYPE;
+    }
+
+    public void setSCH_TYPE(String SCH_TYPE) {
+        this.SCH_TYPE = SCH_TYPE;
+    }
+
+    public String getSUBJECT() {
+        return SUBJECT;
+    }
+
+    public void setSUBJECT(String SUBJECT) {
+        this.SUBJECT = SUBJECT;
+    }
+
+    public String getDEPT_TYPE() {
+        return DEPT_TYPE;
+    }
+
+    public void setDEPT_TYPE(String DEPT_TYPE) {
+        this.DEPT_TYPE = DEPT_TYPE;
+    }
+
+    private String END_DY;
+    private String SCH_TYPE;
+    private String SUBJECT;
+    private String DEPT_TYPE;
+}
--- src/main/java/schedule/scheduleDAO.java
+++ src/main/java/schedule/scheduleDAO.java
@@ -7,6 +7,17 @@ public class scheduleDAO {
     private PreparedStatement pstmt;
     private ResultSet rs;
 
+    public ResultSet getSchedule(){
+        String sql = "SELECT * FROM schedules";
+        try {
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rs;
+    }
+
     public boolean insertSchedule(String START_Y, String START_M, String START_D, String START_DY, String END_Y, String END_M, String END_D, String END_DY, String SCH_TYPE, String SUBJECT, String DEPT_TYPE) {
         String sql = "INSERT INTO reply (START_Y, START_M, START_D, START_DY, END_Y, END_M, END_D, END_DY, SCH_TYPE, SUBJECT, DEPT_TYPE) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         try {
